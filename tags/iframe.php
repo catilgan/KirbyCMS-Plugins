b1cae36aa916a3a104ff85c6a9478e16413c4ddb
Cahit Atilgan <cahit.atilgan82@gmail.com>
2015-11-16 15:48:11 +0100

    Iframe tag supports all default attributes now

--- tags/iframe.php
+++ tags/iframe.php
@@ -1,11 +1,31 @@
 <?php
+
+$iframeAttr = array(
+    'src' => '',
+    'srcdoc' => '',
+    'name' => '',
+    'width' => '100%',
+    'height' => '100px',
+    'align' => 'left',
+    'allowfullscreen' => 'true',
+    'frameborder' => 'false',
+    'longdesc' => '',
+    'marginheight' => '0',
+    'marginwidth' => '0',
+    'scrolling' => 'true',
+    'sandbox' => '',
+    'seamless' => 'true',
+);
+
 kirbytext::$tags['iframe'] = array(
-  'attr' => array(
-    'height'
-  ),
-  'html' => function($tag) {
-    $url	 = $tag->attr('iframe');
-    $height  = $tag->attr('height', '100px');
-	return '<iframe style="width:100%; height:' . $height . '" src="' . $url . '"></iframe>';    
-  }
+    'attr' => array_keys($iframeAttr),
+    'html' => function ($tag) use ($iframeAttr) {
+        $values = [];
+        foreach ($iframeAttr as $attr => $value) {
+            $values[$attr] = $tag->attr($attr, $value);
+        }
+        $values['src'] = $tag->attr('iframe', $values['src']);
+
+        return '<iframe ' . attr($values) . '></iframe>';
+    }
 );
